c089ab9957febb0793c27dae7b844cc54b2c2769
unknown <D:\Program Files (x86)\The Bat!\Mail>
2019-06-08 16:55:21 +0400

    commit

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://admin:aaa123456@ds141654.mlab.com:41654/vuemongo',
-  // mongoURI: 'mongodb://localhost:27017/codehtml-vue',
+  mongoURI: 'mongodb://localhost:27017/codehtml-vue',
   jwt: 'dev-jwt'
 }
